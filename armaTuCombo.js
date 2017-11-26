a471e99d88c650285dd6bb1591f11f87ae8fc5d6
MarCalmon <marical2222@gmail.com>
2017-11-26 09:22:25 -0300

    armado del formulario de pedido

--- armaTuCombo.js
+++ armaTuCombo.js
@@ -2,3 +2,10 @@ var principal= ['tarta de jyq','ensalada caprese','milanesa','1/4 de pollo'];
 var guarnicion = ['ensalada mixta','papas fritas','pure de zapallo'];
 var postre = ['flan con crema','queso y dulce','mousse de chocolate'];
 
+$(document).ready(function(){
+
+	
+
+
+
+});
