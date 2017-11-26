1395a0d7463cdc32f1c65933bcc1b2d3f6d41d0f
MarCalmon <marical2222@gmail.com>
2017-11-26 08:41:25 -0300

    creacion de archivo arma tu combo html y js

---
+++ armaTuCombo.js
@@ -0,0 +1,4 @@
+var principal= ['tarta de jyq','ensalada caprese','milanesa','1/4 de pollo'];
+var guarnicion = ['ensalada mixta','papas fritas','pure de zapallo'];
+var postre = ['flan con crema','queso y dulce','mousse de chocolate'];
+
